a1d67fd293a3d9c6157f7c736ceb1ae94106a0ae
Nafi R <saejim91@gmail.com>
2023-02-11 02:25:28 +0600

    scope operator added

---
+++ scope_resolution.php
@@ -0,0 +1,44 @@
+<?php
+
+ //PARENT CLASS
+
+
+
+ class Vehicle{
+
+ 
+
+    const INDUSTRY = "Automobile";
+
+    public static function printIndustry(){
+          echo "<h3> Parent </h3>";
+        echo "This is from the ". self::class . " class.  This is an " . self::INDUSTRY . " Industry.". "<br>" ;
+    }
+ }
+
+ $vehicle = new Vehicle;
+
+$vehicle::printIndustry();
+
+
+class Truck extends Vehicle{
+
+    public static $staticProperty = "Heavy duty trucks";
+
+    //METHODS
+
+    public static function printInfo(){
+
+        // echo self::$staticProperty;
+        echo "<h3> Child </h3>";
+        echo "This " . self::class . " is from the " . parent::class . " Class";
+        echo "<br>";
+        echo "This class contain all the ". self::$staticProperty;
+
+    }
+
+}
+
+$Nissan_titan = new Truck;
+
+$Nissan_titan::printInfo();

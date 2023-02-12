0a31ff609c9abadb9dbb2e6e4b8daa9546261b31
Nafi R <saejim91@gmail.com>
2023-02-12 20:00:21 +0600

    Implementing classes added

---
+++ classes/interfaces.class.php
@@ -0,0 +1,29 @@
+<?php
+
+class PayPal {
+    public function payNow(){
+        //
+    }
+};
+class Cash {
+     public function payNow(){
+        //
+    }
+};
+class bankTransfer {
+     public function payNow(){
+        //
+    }
+};
+class mobileTransfer {
+     public function payNow(){
+        //
+    }
+};
+
+class Purchase {
+
+    public function pay(){
+
+    }
+}
